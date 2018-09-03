48022938f82081824503e66fbe982b6db936d3f0
Luke Harris <l_harris4@fanshaweonline.ca>
2018-09-03 12:39:00 -0400

    basic splitscreen

--- Aeon/Assets/Scripts/PlayerController.cs
+++ Aeon/Assets/Scripts/PlayerController.cs
@@ -9,33 +9,59 @@ public class PlayerController : MonoBehaviour {
     Rigidbody2D myBody;
     GameObject self;
     Vector2 forward;
+    int playerId;
+    Camera trackingCamera;
+    Vector3 offset = new Vector3(0, 0, -10);
+    UnityEngine.KeyCode forwardKey = KeyCode.W;
+    UnityEngine.KeyCode leftKey = KeyCode.A;
+    UnityEngine.KeyCode rightKey = KeyCode.D;
+    public GameObject playerManagerObject;
+    public PlayerManager playerManager;
+    PlayerManager.Player player;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         acceleration = new Vector2();
-        //self = this.GetComponentInParent<GameObject>();
         myBody = gameObject.GetComponent<Rigidbody2D>();
+
+        playerManagerObject = GameObject.Find("PlayerManagerMain");
+        //get playerId from the PlayerManager
+        playerManager = playerManagerObject.GetComponent<PlayerManager>();
+        playerId = playerManager.AssignPlayer();
+        player = playerManager.players[playerId];
+        forwardKey = player.forward;
+        leftKey = player.left;
+        rightKey = player.right;
+
+        //getting the camera
+        trackingCamera = GameObject.Find("camera" + playerId).GetComponent<Camera>();
+    }
+
+    void Awake()
+    {
+
     }
 	
 	// Update is called once per frame
 	void Update () {
 
+        trackingCamera.transform.position = transform.position + offset;
         float deltaTime = Time.deltaTime;
         bool pressed = false;
         forward = new Vector2(transform.up.x, transform.up.y);
 
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKey(KeyCode.W))
+        if (Input.GetKeyDown(forwardKey) || Input.GetKey(forwardKey))
         {
             acceleration += deltaTime * speed * forward;
             pressed = true;
         }
 
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKey(KeyCode.D))
+        if (Input.GetKeyDown(rightKey) || Input.GetKey(rightKey))
         {
             transform.Rotate(Vector3.forward * -3);
         }
 
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKey(KeyCode.A))
+        if (Input.GetKeyDown(leftKey) || Input.GetKey(leftKey))
         {
             transform.Rotate(Vector3.forward * 3);
         }
@@ -44,9 +70,13 @@ public class PlayerController : MonoBehaviour {
         if (!pressed)
         {
             acceleration *= 0.9f;
+            //set the angular drag to lower while the ship isn't thrusting
+            myBody.angularDrag = 0.1f;
         }
         if (pressed)
         {
+            //set the angular drag to higher while the ship is moving
+            myBody.angularDrag = 3.0f;
             myBody.velocity += acceleration * deltaTime;
         }
     }
---
+++ Aeon/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class PlayerManager : MonoBehaviour {
+    public class Player
+    {
+        int playerId;
+        public UnityEngine.KeyCode forward;
+        public UnityEngine.KeyCode left;
+        public UnityEngine.KeyCode right;
+        public Player(int pId, UnityEngine.KeyCode frwd, UnityEngine.KeyCode lt, UnityEngine.KeyCode rt)
+        {
+            this.playerId = pId;
+            this.forward = frwd;
+            this.left = lt;
+            this.right = rt;
+        }
+    }
+
+    public List<Player> players;
+    public int assignedPlayers;
+	// Use this for initialization
+	void Start () {
+
+    }
+
+    void Awake()
+    {
+        assignedPlayers = 0;
+        players = new List<Player>();
+        players.Add(new Player(0, KeyCode.W, KeyCode.A, KeyCode.D));
+        players.Add(new Player(1, KeyCode.UpArrow, KeyCode.LeftArrow, KeyCode.RightArrow));
+    }
+
+    public int AssignPlayer()
+    {
+        if (assignedPlayers < players.Count)
+        {
+            assignedPlayers++;
+            return assignedPlayers-1;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+
